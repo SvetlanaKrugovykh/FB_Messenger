9e68066f2ddc45e26ae6b27d50936142462db11b
SvetlanaKrugovykh <Svetlana.Krugovykh@gmail.com>
2024-12-08 18:17:14 +0100

    another fix

--- src/utils/webhookHandler.js
+++ src/utils/webhookHandler.js
@@ -3,35 +3,13 @@ const botService = require('../services/messageService')
 exports.processEvent = async (event) => {
   const senderId = event.sender.id
 
-  if (event?.message?.text) {
-    const userMessage = event.message.text
-
-    if (userMessage.toLowerCase().includes('hello')) {
-      await botService.sendTextMessage(senderId, 'Welcome to our bot!')
-      await botService.sendButtonMessage(senderId, 'Choose the action:', [
-        {
-          type: 'postback',
-          title: 'Send request',
-          payload: 'SEND_REQUEST'
-        },
-        {
-          type: 'postback',
-          title: 'Contact us',
-          payload: 'CONTACT_US'
-        }
-      ])
+  try {
+    if (event?.postback?.payload) {
+      const userMessage = event?.postback?.title
+      await botService.sendTextMessage(senderId, userMessage)
     }
-  } else if (event.postback && event.postback.payload) {
-    switch (event.postback.payload) {
-      case 'SEND_REQUEST':
-        await botService.sendTextMessage(senderId, 'You selected to send a request')
-        break
-      case 'CONTACT_US':
-        await botService.sendTextMessage(senderId, 'The contact information is:')
-        break
-      default:
-        await botService.sendTextMessage(senderId, 'Unknown postback payload!')
-    }
+  } catch (error) {
+    console.error('Error processing event:', error)
   }
 }
 
