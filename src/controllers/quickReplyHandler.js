0b8d0a0c1535e8eca703e1d2bdcedafd8512a2c5
SvetlanaKrugovykh <Svetlana.Krugovykh@gmail.com>
2024-12-08 18:31:16 +0100

    transfer

--- src/controllers/quickReplyHandler.js
+++ src/controllers/quickReplyHandler.js
@@ -1,5 +1,4 @@
 const axios = require('axios')
-require('dotenv').config()
 const { sendTxtMsgToTelegram } = require('../services/re-send')
 
 
@@ -12,29 +11,5 @@ exports.handleQuickReply = async (event) => {
   const text = `📌${event.message.text}`
 
   console.log('Postback received:', payload)
-  await sendMessage(senderId, `✅ ${payload}`)
   await sendTxtMsgToTelegram(text, platform, senderId)
-}
-
-async function sendMessage(recipientId, text) {
-  const PAGE_ACCESS_TOKEN = process.env.FACEBOOK_PAGE_ACCESS_TOKEN
-  const url = `https://graph.facebook.com/${process.env.API_VERSION}/me/messages?access_token=${PAGE_ACCESS_TOKEN}`
-
-  const messageData = {
-    recipient: {
-      id: recipientId,
-    },
-    message: {
-      text: text,
-    },
-  }
-
-  try {
-    const response = await axios.post(url, messageData, {
-      headers: { 'Content-Type': 'application/json' },
-    })
-    console.log('Message sent:', response.data)
-  } catch (error) {
-    console.error('Error sending message:', error.response ? error.response.data : error.message)
-  }
 }
